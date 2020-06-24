366e44073ff226f2b0c6e4a3622f9791281e3dea
MasindeOdera <MasindeOdera@gmail.com>
2020-06-24 17:00:09 +0200

    Updates

---
+++ precache-manifest.edaa9172e8c735431be2f0593f65ae95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51a6c58431bf427f89fc74c36dc6937d",
+    "url": "/hangman/index.html"
+  },
+  {
+    "revision": "26263864aafe5239cb3c",
+    "url": "/hangman/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2986ab771b96822977c7",
+    "url": "/hangman/static/js/2.c740e2da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hangman/static/js/2.c740e2da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26263864aafe5239cb3c",
+    "url": "/hangman/static/js/main.97cf25bc.chunk.js"
+  },
+  {
+    "revision": "7ef95293683d0ed87720",
+    "url": "/hangman/static/js/runtime-main.6b115b9b.js"
+  }
+]);
